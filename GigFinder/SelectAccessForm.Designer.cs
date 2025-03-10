d000f2f724e92c138d1f7426b7a3bbc7aa80fc05
Victor <vcortesc2324@politecnics.barcelona>
2025-03-10 20:03:59 +0100

    Creación form menú

--- GigFinder/SelectAccessForm.Designer.cs
+++ GigFinder/SelectAccessForm.Designer.cs
@@ -28,78 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.manageDatabaseButton = new System.Windows.Forms.Button();
-            this.manageUsersButton = new System.Windows.Forms.Button();
-            this.loginLabel = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.customPanelLogin = new GigFinder.Controls.CustomPanel();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // customPanelLogin
             // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.groupBox1.Controls.Add(this.manageDatabaseButton);
-            this.groupBox1.Controls.Add(this.manageUsersButton);
-            this.groupBox1.Controls.Add(this.loginLabel);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(552, 265);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            // 
-            // manageDatabaseButton
-            // 
-            this.manageDatabaseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.manageDatabaseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.manageDatabaseButton.Location = new System.Drawing.Point(117, 152);
-            this.manageDatabaseButton.Name = "manageDatabaseButton";
-            this.manageDatabaseButton.Size = new System.Drawing.Size(341, 49);
-            this.manageDatabaseButton.TabIndex = 7;
-            this.manageDatabaseButton.Text = "Administrar base de datos";
-            this.manageDatabaseButton.UseVisualStyleBackColor = true;
-            this.manageDatabaseButton.Click += new System.EventHandler(this.buttonManageDatabase_Click);
-            // 
-            // manageUsersButton
-            // 
-            this.manageUsersButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.manageUsersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.manageUsersButton.Location = new System.Drawing.Point(149, 48);
-            this.manageUsersButton.Name = "manageUsersButton";
-            this.manageUsersButton.Size = new System.Drawing.Size(273, 49);
-            this.manageUsersButton.TabIndex = 6;
-            this.manageUsersButton.Text = "Administrar usuarios";
-            this.manageUsersButton.UseVisualStyleBackColor = true;
-            this.manageUsersButton.Click += new System.EventHandler(this.buttonManageUsers_Click);
-            // 
-            // loginLabel
-            // 
-            this.loginLabel.AutoSize = true;
-            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginLabel.Location = new System.Drawing.Point(191, 16);
-            this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(0, 29);
-            this.loginLabel.TabIndex = 0;
+            this.customPanelLogin.BackColor = System.Drawing.Color.White;
+            this.customPanelLogin.BorderRadius = 8;
+            this.customPanelLogin.Location = new System.Drawing.Point(300, 50);
+            this.customPanelLogin.Name = "customPanelLogin";
+            this.customPanelLogin.Size = new System.Drawing.Size(600, 550);
+            this.customPanelLogin.TabIndex = 1;
             // 
             // SelectAccessForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 289);
-            this.Controls.Add(this.groupBox1);
+            this.BackgroundImage = global::GigFinder.Properties.Resources.Fondo_escritorio;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.customPanelLogin);
+            this.DoubleBuffered = true;
             this.Name = "SelectAccessForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SelectAccessForm";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label loginLabel;
-        private System.Windows.Forms.Button manageUsersButton;
-        private System.Windows.Forms.Button manageDatabaseButton;
+        private Controls.CustomPanel customPanelLogin;
     }
 }
